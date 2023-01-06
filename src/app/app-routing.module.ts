447fcdafdbc2a9f35d523ed2e004b1afe41fe475
benlakhdaramine@gmail.com <benlakhdaramine@gmail.com>
2023-01-06 16:19:05 +0100

    menu en desktop ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './_Pages/accueil/accueil.component';
+import { ProduitsComponent } from './_Pages/produits/produits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccueilComponent,
+  children: [
+    { path: 'produits', component: ProduitsComponent},
+/*     {path: '', component: PageXComponent }, */
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './_Components/header/header.component';
+import { MainComponent } from './_Components/main/main.component';
+import { FooterComponent } from './_Components/footer/footer.component';
+import { ProduitsComponent } from './_Pages/produits/produits.component';
+import { AccueilComponent } from './_Pages/accueil/accueil.component';
+import { MainProductsComponent } from './_Components/main-products/main-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ProduitsComponent,
+    AccueilComponent,
+    MainProductsComponent
   ],
   imports: [
     BrowserModule,
