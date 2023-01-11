4144ca79ea3dbfc6c0ec752bcb6b8e7773e9b31c
benlakhdaramine@gmail.com <benlakhdaramine@gmail.com>
2023-01-11 11:08:57 +0100

    page livres/id  ok

--- src/app/modules/livres/pages/livre/livre.component.ts
+++ src/app/modules/livres/pages/livre/livre.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LivreService } from 'src/app/core/services/livreService/livre.service';
@@ -12,12 +11,12 @@ import { Livre } from 'src/app/core/_model/livre';
 export class LivreComponent {
 
 
-  constructor(private route:ActivatedRoute, private httpClient:HttpClient,private livre:LivreService) {
+  constructor(private route:ActivatedRoute,private livre:LivreService) {
     this.id = this.route.snapshot.params['id'];
   }
   id!: number;
   book!: Livre;
-  coverPath!: string;
+
   forimg = "assets/images/livres/"
   ngOnInit(): void {
     this.livre.getLivre(this.id).subscribe((book) => (this.book = book));
